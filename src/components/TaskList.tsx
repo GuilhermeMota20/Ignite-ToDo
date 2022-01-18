7b5db4c4feed4ece64aeaf7531f6d24f3770b07f
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2022-01-18 14:58:59 -0300

    :tada: challenge finished!

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return; // se estiver vazio, não cria uma nova task;
+
+    const newTask = {
+      id: Math.random(), // id aleatória;
+      title: newTaskTitle, // passando o valor do input para chave title;
+      isComplete: false // estado inicial da tesk;
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
